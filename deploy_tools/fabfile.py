40c4da9f9ea95232692b8fb6890b2285530ab70f
Chris Walsh <taibhsehat@gmail.com>
2019-04-01 23:52:52 +0200

    fixed the fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,8 +5,10 @@ from fabric.api import cd, env, local, run
 REPO_URL = 'https://github.com/taibhse/blog.git'
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-	run(f'mkdir -p {site_folder}')  
+    print("Executing on %s as %s" % (env.host, env.user))
+    #site_folder = f'/home/{env.user}/sites/{env.host}'
+    site_folder = "/home/%s/sites/%s" % (env.user, env.host)
+    run("mkdir -p %s" % (site_folder))  
     with cd(site_folder):  
         _get_latest_source()
         _update_virtualenv()
@@ -15,27 +17,27 @@ def deploy():
         _update_database()
 
 def _get_latest_source():
-	if exists('.git'):
-		run('git fetch')
-	else:
-		run(f'git clone {REPO_URL} .')
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run('git clone {REPO_URL} .')
         current_commit = local("git log -n 1 --format=%H", capture=True)  
-        run(f'git reset --hard {current_commit}')
+        run('git reset --hard {current_commit}')
 
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):  
-        run(f'python3.6 -m venv virtualenv')
+        run('python3 -m venv virtualenv')
     run('./virtualenv/bin/pip install -r requirements.txt')
 
 def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')  
-    append('.env', f'SITENAME={env.host}')
+    append('.env', 'SITENAME={env.host}')
     current_contents = run('cat .env')  
     if 'DJANGO_SECRET_KEY' not in current_contents:  
         new_secret = ''.join(random.SystemRandom().choices(  
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        append('.env', 'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
